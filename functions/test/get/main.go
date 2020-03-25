e2f91865b62549a123901cb3e79dfd35ea1757e4
Maisie <maisiesadler@hotmail.com>
2020-03-25 20:09:50 +0000

    Log attributes

--- functions/test/get/main.go
+++ functions/test/get/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -15,15 +16,35 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	// stdout and stderr are sent to AWS CloudWatch Logs
 	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
 
+	logAttributes(&request)
+
 	resp := ResponseSuccessful("Hello, world")
 	return resp, nil
+}
+
+func logAttributes(request *events.APIGatewayProxyRequest) {
+
+	for k, v := range request.RequestContext.Authorizer {
+		fmt.Printf("Got k='%v', v='%v'\n", k, v)
+	}
+
+	if claims, ok := request.RequestContext.Authorizer["claims"]; ok {
+		c := claims.(map[string]interface{})
+		// username, ok := c["cognito:username"]
+		// return username.(string), ok
+
+		for k, v := range c {
+			fmt.Printf("Claims has k='%v', v='%v'\n", k, v)
+		}
+	}
+
 }
 
 // ResponseSuccessful returns a 200 response for API Gateway that allows cors
 func ResponseSuccessful(body string) events.APIGatewayProxyResponse {
 	resp := events.APIGatewayProxyResponse{Headers: make(map[string]string)}
 	resp.Headers["Access-Control-Allow-Origin"] = "*"
-	resp.Headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization,dfd-auth"
+	resp.Headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
 	resp.Body = body
 	resp.StatusCode = 200
 	return resp
